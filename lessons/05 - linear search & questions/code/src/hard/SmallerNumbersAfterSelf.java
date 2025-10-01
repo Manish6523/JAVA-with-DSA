d056afd9b191f6a767a6ea2429dada32eeb98182
Manish6523 <220101005092@gandhinagaruni.ac.in>
2025-10-01 20:24:21 +0530

    2nd hard question

--- lessons/05 - linear search & questions/code/src/hard/SmallerNumbersAfterSelf.java
+++ lessons/05 - linear search & questions/code/src/hard/SmallerNumbersAfterSelf.java
@@ -1,4 +1,57 @@
 package hard;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class SmallerNumbersAfterSelf {
+    public static void main(String[] args) {
+        /*
+        Given an integer array nums, return an integer array counts where counts[i] is the number of smaller elements to the right of nums[i].
+
+        Example 1:
+
+        Input: nums = [5,2,6,1]
+        Output: [2,1,1,0]
+
+        Explanation:
+        To the right of 5 there are 2 smaller elements (2 and 1).
+        To the right of 2 there is only 1 smaller element (1).
+        To the right of 6 there is 1 smaller element (1).
+        To the right of 1 there is 0 smaller element.
+         */
+        int[] que = {5,2,6,1};
+        System.out.println(countSmaller(que));
+    }
+    static List<Integer> countSmaller(int[] nums) {
+
+        ArrayList<Integer> ans = new ArrayList<>();
+        ArrayList<Integer> sorted = new ArrayList<>();
+
+        for (int i = nums.length-1; i >= 0 ; i--) {
+            int idx = sp(sorted, nums[i]);
+            sorted.add(idx,nums[i]);
+            ans.add(idx);
+        }
+        return ans.reversed();
+    }
+
+    static int sp(ArrayList<Integer> arr, int value) {
+        int start = 0;
+        int end = arr.size() - 1;
+
+        while (start <= end) { // use <= to include end element
+            int mid = start + (end - start) / 2; // safer than (start+end)/2
+            if (arr.get(mid) == value) {
+                return mid; // value found, return its index
+            } else if (arr.get(mid) < value) {
+                start = mid + 1; // search right half
+            } else {
+                end = mid - 1; // search left half
+            }
+        }
+        // if not found, start will be the correct insertion index
+        return start;
+    }
+
 }
--- lessons/05 - linear search & questions/code/src/medium/KokoBananaProblem.java
+++ lessons/05 - linear search & questions/code/src/medium/KokoBananaProblem.java
@@ -1,4 +1,50 @@
 package medium;
 
 public class KokoBananaProblem {
+    public static void main(String[] args) {
+        /*
+        Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
+        Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
+        Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
+        Return the minimum integer k such that she can eat all the bananas within h hours.
+
+        Example 1:
+        Input: piles = [3,6,7,11], h = 8
+        Output: 4
+         */
+
+        // Test Case 1 (LeetCode Example)
+        int[] arr1 = {30,11,23,4,20};
+        int hrs1 = 6;
+        System.out.println(minEatingSpeed(arr1, hrs1)); // Expected: 4
+
+    }
+
+    static int minEatingSpeed(int[] piles, int h) {
+        int start = 1;
+        int end = piles[0];
+
+        for(int n : piles){
+            if (n>end){
+                end = n;
+            }
+        }
+        int ans = end;
+
+        while (start<=end){
+            long count = 0;
+            int mid = start + (end - start) / 2;
+
+            for(int b : piles){
+                count += (b + (long) mid - 1) / mid;
+            }
+            if(count<=h){
+                ans = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return ans; // both start and end are same.
+    }
 }
