209c7dd542077444b70c1fe1e6acebfd331d1492
Manish6523 <220101005092@gandhinagaruni.ac.in>
2025-10-05 00:01:14 +0530

    selection & bubble sort

--- lessons/06 - Sorting Algorithms/untitled/src/Algorithms/BubbleSort.java
+++ lessons/06 - Sorting Algorithms/untitled/src/Algorithms/BubbleSort.java
@@ -1,4 +1,29 @@
 package Algorithms;
 
+import java.util.Arrays;
+
 public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {2,4,3,1,0};
+        bubble(arr);
+        System.out.println("Bubble sort : " + Arrays.toString(arr));
+    }
+
+    static void bubble(int[] arr){
+        boolean isSwapped = false;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 1; j < arr.length - i; j++) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                    isSwapped = true;
+                }
+            }
+            System.out.println("After pass " + (i + 1) + ": " + Arrays.toString(arr));
+            if (!isSwapped){
+                break;
+            }
+        }
+    }
 }
--- lessons/06 - Sorting Algorithms/untitled/src/Algorithms/SelectionSort.java
+++ lessons/06 - Sorting Algorithms/untitled/src/Algorithms/SelectionSort.java
@@ -1,4 +1,29 @@
 package Algorithms;
 
+import java.util.Arrays;
+
 public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {5,4,3,2,1};
+        selection(arr);
+        System.out.println("Selection sort : " + Arrays.toString(arr));
+    }
+
+    static void selection(int[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+            int max = 0;
+            for (int j = 0; j < arr.length - i; j++) {
+                if (arr[max] < arr[j]){
+                    max = j;
+                }
+            }
+            // swap
+            int temp = arr[max];
+            arr[max] = arr[arr.length - i - 1];
+            arr[arr.length - i - 1] = temp;
+            System.out.println("After pass " + (i + 1) + ": " + Arrays.toString(arr));
+
+        }
+    }
 }
