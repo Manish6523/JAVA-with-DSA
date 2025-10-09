8f14f720d4db420a65046c212bcde6fc8a175bb5
Manish6523 <220101005092@gandhinagaruni.ac.in>
2025-10-09 22:55:06 +0530

    Easy questions in sorting

--- lessons/06 - Sorting Algorithms/code/src/Algorithms/InsertionSort.java
+++ lessons/06 - Sorting Algorithms/code/src/Algorithms/InsertionSort.java
@@ -1,4 +1,25 @@
 package Algorithms;
 
+import java.util.Arrays;
+
 public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {2,1};
+        insertion(arr);
+        System.out.println("Insertion sort : " + Arrays.toString(arr));
+    }
+
+    static void insertion(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i+1; j > 0; j--) {
+                if (arr[j]<arr[j-1]){
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                } else break;
+            }
+            System.out.println("After pass " + (i + 1) + ": " + Arrays.toString(arr));
+        }
+    }
 }
